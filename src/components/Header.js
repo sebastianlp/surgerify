4853529affd3dcf9cdf220a3246f6e8b2fc9292e
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2020-02-20 22:40:51 -0300

    Split layout

---
+++ src/components/Header.js
@@ -0,0 +1,91 @@
+import React from "react";
+import clsx from "clsx";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+import { useAuth } from "context/AuthContext";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: theme.drawerWidth,
+    width: `calc(100% - ${theme.drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  logoutButton: {
+    color: theme.palette.common.white
+  }
+}));
+
+function Header(props) {
+  const classes = useStyles();
+  const { logout } = useAuth();
+
+  return (
+    <AppBar
+      position="absolute"
+      className={clsx(classes.appBar, props.open && classes.appBarShift)}
+    >
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={props.onDrawerCloseIconClick}
+          className={clsx(
+            classes.menuButton,
+            props.open && classes.menuButtonHidden
+          )}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          className={classes.title}
+        >
+          Surgerify
+        </Typography>
+        <Button
+          onClick={logout}
+          className={classes.logoutButton}
+          endIcon={<ExitToAppIcon />}
+        >
+          Salir
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,123 @@
+import React from "react";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import PersonIcon from "@material-ui/icons/Person";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import IconButton from "@material-ui/core/IconButton";
+import Divider from "@material-ui/core/Divider";
+
+import { useUser } from "context/UserContext";
+
+const useStyles = makeStyles(theme => ({
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: theme.drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  }
+}));
+
+function Sidebar(props) {
+  const classes = useStyles();
+  const user = useUser();
+
+  return (
+    <Drawer
+      variant="permanent"
+      classes={{
+        paper: clsx(
+          classes.drawerPaper,
+          !props.open && classes.drawerPaperClose
+        )
+      }}
+      open={props.open}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={props.onDrawerCloseIconClick}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <PersonIcon />
+          </ListItemIcon>
+          <ListItemText primary={`Hola ${user.displayName}!`} />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        {Links().map(link => (
+          <ListItemLink
+            key={link.key}
+            icon={link.icon}
+            primary={link.primary}
+            to={link.to}
+          />
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+function ListItemLink(props) {
+  const { icon, primary, to } = props;
+
+  const renderRouterLink = React.useMemo(
+    () =>
+      React.forwardRef((linkProps, ref) => (
+        <Link ref={ref} to={to} {...linkProps} />
+      )),
+    [to]
+  );
+
+  return (
+    <ListItem button component={renderRouterLink}>
+      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemText primary={primary} />
+    </ListItem>
+  );
+}
+
+function Links() {
+  return [
+    {
+      key: "surgeries",
+      to: "/surgeries",
+      icon: <LocalHospitalIcon />,
+      primary: "Cirugías"
+    }
+  ];
+}
+
+export default Sidebar;
